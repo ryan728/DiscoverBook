92b1cbe4054426ad51c5eeb877d9ddbdf15b92b7
leizhang <zhanglei728@gmail.com>
2012-06-20 10:30:22 +0800

    add discover book project and douban-objc-client submodule

---
+++ DiscoverBook/DiscoverBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiscoverBook
+//
+//  Created by Lei Zhang on 6/19/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
