8916912955701b1bae7ae953ede3f070b062e75b
leizhang <zhanglei728@gmail.com>
2012-07-02 18:53:53 +0800

    fix bugs

--- DiscoverBook/src/MyBookController.m
+++ DiscoverBook/src/MyBookController.m
@@ -15,9 +15,13 @@
 
 @end
 
-@implementation MyBookController
+@implementation MyBookController {
+  NSUInteger currentIndex_;
+  BOOL hasMore_;
+}
 
 static UIImage *DEFAULT_BOOK_COVER_IMAGE = nil;
+static NSUInteger RESULT_BATCH_SIZE = 10;
 
 + (void)initialize {
   NSString *defaultBookCoverPath = [[NSBundle mainBundle] pathForResource:@"default_book_cover" ofType:@"jpg"];
@@ -35,18 +39,25 @@ static UIImage *DEFAULT_BOOK_COVER_IMAGE = nil;
 
   self.clearsSelectionOnViewWillAppear = NO;
   readingBooks_ = [[NSMutableArray alloc] init];
+  currentIndex_ = 0;
+  hasMore_ = YES;
   if ([User defaultUser]) {
-    [self initReadingList];
+    [self loadReadingListFrom:0];
   } else {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initReadingList) name:@"UserInfoFetched" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(defaultUserFetched:) name:@"UserInfoFetched" object:nil];
   }
   // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
   // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)initReadingList {
+- (void)defaultUserFetched:(NSNotification *)notification {
+  [self loadReadingListFrom:0];
+}
+
+
+- (void)loadReadingListFrom:(int)startIndex {
   User *user = [User defaultUser];
-  NSDictionary *const parameters = [NSDictionary dictionaryWithObjects:Array(@"book", self.title, @"20") forKeys:Array(@"cat", @"status", @"max-results")];
+  NSDictionary *const parameters = [NSDictionary dictionaryWithObjects:Array(@"book", self.title, [NSString stringWithFormat:@"%u", RESULT_BATCH_SIZE], [NSString stringWithFormat:@"%u", startIndex]) forKeys:Array(@"cat", @"status", @"max-results", @"start-index")];
   DOUQuery *const bookQuery = [[DOUQuery alloc] initWithSubPath:[NSString stringWithFormat:@"/people/%@/collection", user.id] parameters:parameters];
 
   DOUService *service = [DOUService sharedInstance];
@@ -62,6 +73,10 @@ static UIImage *DEFAULT_BOOK_COVER_IMAGE = nil;
         DoubanEntrySubject *subject = [[DoubanEntrySubject alloc] initWithXMLElement:[[[entry XMLElement] elementsForName:@"db:subject"] objectAtIndex:0] parent:nil];
         [readingBooks_ addObject:subject];
       }];
+
+      if (entries.count != RESULT_BATCH_SIZE) {
+        hasMore_ = NO;
+      }
 
       [[self tableView] reloadData];
       [[self tableView] layoutIfNeeded];
@@ -79,41 +94,54 @@ static UIImage *DEFAULT_BOOK_COVER_IMAGE = nil;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section; {
+  if (readingBooks_.count == 0) {
+    return 0;
+  }
   return readingBooks_.count;
+//  return hasMore_ ? readingBooks_.count + 1 : readingBooks_.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath; {
-  static NSString *CellIdentifier = @"READING_BOOK_ITEM";
+  NSString *CellIdentifier = @"READING_BOOK_ITEM";
+  NSLog(@"indexPath.row = %li", indexPath.row);
 
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 
+   
   if (cell == nil) {
     cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    if (indexPath.row == readingBooks_.count) {
+      cell.textLabel.text = @"Load more ...";
+      UIActivityIndicatorView *const indicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+      [cell addSubview:indicatorView];
+    } else {
+      cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 
-    DoubanEntrySubject *book = [readingBooks_ objectAtIndex:indexPath.row];
-    cell.textLabel.text = book.title.stringValue;
+      DoubanEntrySubject *book = [readingBooks_ objectAtIndex:indexPath.row];
+      NSLog(@"book.identifier = %@", book.identifier);
+      cell.textLabel.text = book.title.stringValue;
 
-    NSURL *const imageUrl = [[book linkWithRelAttributeValue:@"image"] URL];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:imageUrl cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0];
-    [cell.imageView setImageWithURLRequest:request placeholderImage:DEFAULT_BOOK_COVER_IMAGE success:^void(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-      cell.imageView.alpha = 0.0;
-      [UIView animateWithDuration:1.0 animations:^(void) {
-        cell.imageView.alpha = 1.0f;
+      NSURL *const imageUrl = [[book linkWithRelAttributeValue:@"image"] URL];
+      NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:imageUrl cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0];
+      [cell.imageView setImageWithURLRequest:request placeholderImage:DEFAULT_BOOK_COVER_IMAGE success:^void(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        cell.imageView.alpha = 0.0;
+        [UIView animateWithDuration:1.0 animations:^(void) {
+          cell.imageView.alpha = 1.0f;
+        }];
+      } failure:nil];
+
+      NSMutableString *authors = [NSMutableString string];
+      [book.authors each:^(GDataAtomAuthor *author) {
+        [authors appendString:author.name];
       }];
-    } failure:nil];
-
-    NSMutableString *authors = [NSMutableString string];
-    [book.authors each:^(GDataAtomAuthor *author) {
-      [authors appendString:author.name];
-    }];
-    cell.detailTextLabel.text = authors;
-  }
+      cell.detailTextLabel.text = authors;
+    }
+  
   return cell;
 }
 
 #pragma mark - Table view delegate
-
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 }
 
