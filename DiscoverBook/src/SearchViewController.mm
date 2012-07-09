363cb8f3696bc5b6631f656e3c2d220b46193bba
leizhang <zhanglei728@gmail.com>
2012-07-09 11:48:46 +0800

    add bounds and drag effect

--- DiscoverBook/src/SearchViewController.mm
+++ DiscoverBook/src/SearchViewController.mm
@@ -10,7 +10,11 @@
   [super viewDidLoad];
 
   CCDirector *director = [CCDirector sharedDirector];
-  EAGLView *glView = [EAGLView viewWithFrame:self.view.bounds
+  const CGPoint origin = self.view.bounds.origin;
+  const CGSize navigationBarSize = self.navigationController.navigationBar.bounds.size;
+  const CGSize viewSize = self.view.bounds.size;
+  const CGRect glViewBounds = CGRectMake(origin.x, origin.y + navigationBarSize.height, viewSize.width, viewSize.height - navigationBarSize.height);
+  EAGLView *glView = [EAGLView viewWithFrame:glViewBounds
                                  pixelFormat:kEAGLColorFormatRGB565
                                  depthFormat:0];
   [director setOpenGLView:glView];
--- DiscoverBook/src/SearchViewLayer.mm
+++ DiscoverBook/src/SearchViewLayer.mm
@@ -5,7 +5,6 @@
 #import "CCLabelTTF.h"
 #import "CGPointExtension.h"
 #import "CCActionInterval.h"
-#include <Box2D/Box2D.h>
 
 
 #define PTM_RATIO 32
@@ -20,6 +19,7 @@ enum {
   NSMutableArray *movableSprites_;
   CCSprite *selSprite;
   b2Body *groundBody;
+  BOOL hasTouches;
 }
 
 + (CCScene *)scene {
@@ -35,64 +35,62 @@ enum {
     movableSprites_ = [[NSMutableArray alloc] init];
     self.isTouchEnabled = YES;
     self.isAccelerometerEnabled = YES;
-    
+    hasTouches = NO;
+
     CGSize screenSize = [CCDirector sharedDirector].winSize;
-    NSLog(@"NSStringFromCGSize(screenSize) = %@", NSStringFromCGSize(screenSize));
-    
+
     b2Vec2 gravity;
+//    gravity.Set(0.0f, -9.8f);
     gravity.Set(0.0f, 0.0f);
-    
+
     bool doSleep = true;
     world = new b2World(gravity);
     world->SetAllowSleeping(doSleep);
     world->SetContinuousPhysics(true);
     m_debugDraw = new GLESDebugDraw(PTM_RATIO);
     world->SetDebugDraw(m_debugDraw);
-    
+
     uint32 flags = 0;
     flags += b2DebugDraw::e_shapeBit;
+    flags += b2DebugDraw::e_jointBit;
     m_debugDraw->SetFlags(flags);
-    
+
     b2BodyDef groundBodyDef;
     groundBodyDef.position.Set(0, 0); // bottom-left corner
-    
+
     groundBody = world->CreateBody(&groundBodyDef);
-    
+
     b2EdgeShape groundBox;
-    
-    
     groundBox.Set(b2Vec2(0, 0), b2Vec2(screenSize.width / PTM_RATIO, 0));
     groundBody->CreateFixture(&groundBox, 0);
-    
+
     // top
     groundBox.Set(b2Vec2(0, screenSize.height / PTM_RATIO), b2Vec2(screenSize.width / PTM_RATIO, screenSize.height / PTM_RATIO));
     groundBody->CreateFixture(&groundBox, 0);
-    
+
     // left
     groundBox.Set(b2Vec2(0, screenSize.height / PTM_RATIO), b2Vec2(0, 0));
     groundBody->CreateFixture(&groundBox, 0);
-    
+
     // right
     groundBox.Set(b2Vec2(screenSize.width / PTM_RATIO, screenSize.height / PTM_RATIO), b2Vec2(screenSize.width / PTM_RATIO, 0));
     groundBody->CreateFixture(&groundBox, 0);
-    
-    
-    
+
     CCSpriteBatchNode *batch = [CCSpriteBatchNode batchNodeWithFile:@"default_book_cover.jpg" capacity:150];
     [self addChild:batch z:0 tag:kTagBatchNode];
-    
+
     b2Body *body1 = [self addNewSpriteWithCoords:ccp(screenSize.width / 2, screenSize.height / 2)];
     b2Body *body2 = [self addNewSpriteWithCoords:ccp(screenSize.width / 3, screenSize.height / 3)];
     [self joinBody:body1 with:body2];
-    
-    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
-    [self addChild:label z:0];
-    [label setColor:ccc3(0, 0, 255)];
-    label.position = ccp(screenSize.width / 2, screenSize.height - 50);
-    
+
+//    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
+//    [self addChild:label z:0];
+//    [label setColor:ccc3(0, 0, 255)];
+//    label.position = ccp(screenSize.width / 2, screenSize.height - 50);
+
     [self schedule:@selector(tick:)];
   }
-  
+
   return self;
 }
 
@@ -103,20 +101,29 @@ enum {
   glDisable(GL_TEXTURE_2D);
   glDisableClientState(GL_COLOR_ARRAY);
   glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  
+
   world->DrawDebugData();
-  
+
   // restore default GL states
   glEnable(GL_TEXTURE_2D);
   glEnableClientState(GL_COLOR_ARRAY);
   glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  
+
 }
 
 - (void)joinBody:(b2Body *)body1 with:(b2Body *)body2 {
-  
+  b2DistanceJointDef distJDef;
+  b2Vec2 anchor1 = body1->GetWorldCenter();
+  b2Vec2 anchor2 = body2->GetWorldCenter();
+  distJDef.Initialize(body1, body2, anchor1, anchor2);
+  distJDef.collideConnected = false;
+  distJDef.dampingRatio = 0.9f;
+  distJDef.frequencyHz = 3;
+  distJDef.length = (body1->GetPosition() - body2->GetPosition()).Length() * 1.5;
+  world->CreateJoint(&distJDef);
+
   b2RopeJointDef rDef;
-  rDef.maxLength = (body1->GetPosition() - body2->GetPosition()).Length() * 2;
+  rDef.maxLength = (body1->GetPosition() - body2->GetPosition()).Length() * 2.5;
   rDef.localAnchorA = rDef.localAnchorB = b2Vec2_zero;
   rDef.bodyA = body2;
   rDef.bodyB = body1;
@@ -126,39 +133,40 @@ enum {
 - (b2Body *)addNewSpriteWithCoords:(CGPoint)p {
   CCLOG(@"Add sprite %0.2f x %02.f", p.x, p.y);
   CCSpriteBatchNode *batch = (CCSpriteBatchNode *) [self getChildByTag:kTagBatchNode];
-  
+
   //We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
   //just randomly picking one of the images
   int idx = (CCRANDOM_0_1() > .5 ? 0 : 1);
   int idy = (CCRANDOM_0_1() > .5 ? 0 : 1);
-  
+
   CCSprite *sprite = [CCSprite spriteWithBatchNode:batch rect:CGRectMake(32 * idx, 32 * idy, 32, 32)];
   [movableSprites_ addObject:sprite];
   [batch addChild:sprite];
-  
+
   sprite.position = ccp( p.x, p.y);
-  
+
   // Define the dynamic body.
   //Set up a 1m squared box in the physics world
   b2BodyDef bodyDef;
   bodyDef.type = b2_dynamicBody;
-  
+
   bodyDef.position.Set(p.x / PTM_RATIO, p.y / PTM_RATIO);
   bodyDef.userData = sprite;
   b2Body *body = world->CreateBody(&bodyDef);
-  
+
   // Define another box shape for our dynamic body.
   b2PolygonShape dynamicBox;
   dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
-  
+
   // Define the dynamic body fixture.
   b2FixtureDef fixtureDef;
   fixtureDef.shape = &dynamicBox;
   fixtureDef.density = 1.0f;
   fixtureDef.friction = 0.3f;
+  fixtureDef.restitution = 0.0f;
   body->CreateFixture(&fixtureDef);
-  body->SetLinearDamping(0.0005f);
-  
+  body->SetLinearDamping(600.0f);
+
   return body;
 }
 
@@ -168,22 +176,29 @@ enum {
   //of the simulation, however, we are using a variable time step here.
   //You need to make an informed choice, the following URL is useful
   //http://gafferongames.com/game-physics/fix-your-timestep/
-  
+
   int32 velocityIterations = 8;
   int32 positionIterations = 1;
-  
+
   // Instruct the world to perform a single step of simulation. It is
   // generally best to keep the time step and iterations fixed.
   world->Step(dt, velocityIterations, positionIterations);
-  
-  
+
+
   //Iterate over the bodies in the physics world
   for (b2Body *b = world->GetBodyList(); b; b = b->GetNext()) {
     if (b->GetUserData() != NULL) {
       //Synchronize the AtlasSprites position and rotation with the corresponding body
-      //      CCSprite *myActor = (CCSprite *) b->GetUserData();
-      //      myActor.position = CGPointMake(b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO);
-      //      myActor.rotation = -1 * CC_RADIANS_TO_DEGREES(b->GetAngle());
+      CCSprite *myActor = (CCSprite *) b->GetUserData();
+      if (myActor == selSprite && hasTouches) {
+        b->SetTransform(b2Vec2(myActor.position.x / PTM_RATIO, myActor.position.y / PTM_RATIO), 0);
+        b->SetActive(true);
+      } else {
+        myActor.position = CGPointMake(b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO);
+        myActor.rotation = -1 * CC_RADIANS_TO_DEGREES(b->GetAngle());
+      }
+//      id action = [CCMoveTo actionWithDuration:0.4 position:CGPointMake( b->GetPosition().x * PTM_RATIO, b->GetPosition().y * PTM_RATIO)];
+//      [sprite runAction:action];
     }
   }
 }
@@ -211,6 +226,7 @@ enum {
 - (void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
   CGPoint touchLocation = [self convertTouchToNodeSpace:[touches anyObject]];
   [self selectSpriteForTouch:touchLocation];
+  hasTouches = YES;
   NSLog(@"---------------- start position) = %@", NSStringFromCGPoint(selSprite.position));
 }
 
@@ -223,9 +239,31 @@ enum {
   return retval;
 }
 
+- (BOOL)spriteOutOfBound:(CGPoint)point {
+  const CGSize spriteSize = selSprite.boundingBox.size;
+  const CGSize winSize = [CCDirector sharedDirector].winSize;
+  if (point.y + spriteSize.height / 2 > winSize.height) {
+    return YES;
+  }
+  if (point.y - spriteSize.height / 2 < 0) {
+    return YES;
+  }
+  if (point.x + spriteSize.width / 2 > winSize.width) {
+    return YES;
+  }
+  if (point.x - spriteSize.width / 2 < 0) {
+    return YES;
+  }
+  return NO;
+}
+
 - (void)panForTranslation:(CGPoint)translation {
   if (selSprite) {
     CGPoint newPos = ccpAdd(selSprite.position, translation);
+
+    if ([self spriteOutOfBound:newPos]) {
+      return;
+    }
     selSprite.position = newPos;
   }
   //  else {
@@ -235,14 +273,13 @@ enum {
 }
 
 - (void)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-  NSLog(@"------------------ touch moved");
   UITouch *touch = [touches anyObject];
   CGPoint touchLocation = [self convertTouchToNodeSpace:touch];
-  
+
   CGPoint oldTouchLocation = [touch previousLocationInView:touch.view];
   oldTouchLocation = [[CCDirector sharedDirector] convertToGL:oldTouchLocation];
   oldTouchLocation = [self convertToNodeSpace:oldTouchLocation];
-  
+
   CGPoint translation = ccpSub(touchLocation, oldTouchLocation);
   [self panForTranslation:translation];
 }
@@ -250,6 +287,7 @@ enum {
 - (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
   //  Add a new body/atlas sprite at the touched location
   NSLog(@"---------------- end position) = %@", NSStringFromCGPoint(selSprite.position));
+  hasTouches = NO;
   //  for (UITouch *touch in touches) {
   //    CGPoint location = [touch locationInView:[touch view]];
   //
@@ -261,20 +299,20 @@ enum {
 
 - (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration {
   static float prevX = 0, prevY = 0;
-  
+
   //#define kFilterFactor 0.05f
 #define kFilterFactor 1.0f  // don't use filter. the code is here just as an example
-  
+
   float accelX = (float) acceleration.x * kFilterFactor + (1 - kFilterFactor) * prevX;
   float accelY = (float) acceleration.y * kFilterFactor + (1 - kFilterFactor) * prevY;
-  
+
   prevX = accelX;
   prevY = accelY;
-  
+
   // accelerometer values are in "Portrait" mode. Change them to Landscape left
   // multiply the gravity by 10
   b2Vec2 gravity(-accelY * 10, accelX * 10);
-  
+
   world->SetGravity(gravity);
 }
 
